316b4cf3f1ffd61c57a495701775ab5ce128861c
trekexol <39245161+trekexol@users.noreply.github.com>
2018-11-18 01:50:23 -0400

    Back End
    funciona login, registra

--- ProyectoPaquetes/src/main/java/com/proyectoPaquetes/repository/ClienteRepository.java
+++ ProyectoPaquetes/src/main/java/com/proyectoPaquetes/repository/ClienteRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.proyectoPaquetes.model.Cliente;
 
+//import org.springframework.data.repository.CrudRepository;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Cliente findByCorreoElectronico(String email);
+    boolean  existsByCorreoElectronico(String email);
+    Cliente findFirstByCorreoElectronicoIgnoreCaseContaining(String partialEmailAddress);
 }
